c6cc545cb31f7dffae0ff8f3f3bf1efa53148d6a
natalia378 <105983586+natalia378@users.noreply.github.com>
2022-05-28 19:05:25 +0200

    Update visualizations.R

--- visualizations.R
+++ visualizations.R
@@ -27,3 +27,24 @@ ggplot(res) +
   geom_point(aes(x = time, y = price, color = if_executed)) +
   geom_hline(yintercept = K)
 
+
+#Wrażliwosć ceny na delta_t
+Delta_t<-c(T/(1:30)) 
+put_e_price<-c()
+put_a_price<-c()
+call_e_price<-c()
+call_a_price<-c()
+for ( i in Delta_t){
+  u=exp(sigma*sqrt(i))
+  d=exp(-sigma*sqrt(i))
+  a<-dim(analize_opt(S0, K, T,i,u,d, r, call_opt = F, opt_type = "E"))[1]
+  put_e_price[which(Delta_t==i)]<-analize_opt(S0, K, T,i,u,d,r, call_opt = FALSE, opt_type = "E")[a,2]
+  put_a_price[which(Delta_t==i)]<-analize_opt(S0, K, T,i,u,d,r, call_opt = FALSE, opt_type = "A")[a,2]
+  call_e_price[which(Delta_t==i)]<-analize_opt(S0, K, T,i,u,d,r, call_opt = TRUE, opt_type = "E")[a,2]
+  call_a_price[which(Delta_t==i)]<-analize_opt(S0, K, T,i,u,d,r, call_opt = TRUE, opt_type = "A")[a,2]
+}
+
+pricing_time1<-data.frame(steps=rep(c(1:30),4),type=rep(c("european put","american put","european call","american call"),each=30),price=c(put_e_price,put_a_price,call_e_price,call_a_price))
+pricing_time1 # tabelka do wykresu 
+ggplot(pricing_time1,aes(x=steps,y=price,color=type,alpha=0.3))+geom_line(size=1,position=position_jitter(w=0.05, h=0.04))+theme_bw()+ggtitle("Wrażliwość ceny opcji na liczbę kroków")
+
