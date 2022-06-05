139049df43b17e15ecd1b07fe51a3dd6d2155bc8
natalia378 <105983586+natalia378@users.noreply.github.com>
2022-06-05 21:46:39 +0200

    Update visualizations.R

--- visualizations.R
+++ visualizations.R
@@ -138,7 +138,7 @@ for ( i in Delta_t){
 library(ggplot2)
 pricing_time2<-data.frame(liczba_kroków=rep(c(1:150),4),typ=rep(c("europejski put","amerykański put","europejski call","amerykański call"),each=150),cena=c(put_e_price,put_a_price,call_e_price,call_a_price))
 pricing_time2# tabelka do wykresu
-ggplot(pricing_time2,aes(x=liczba_kroków,y=cena,color=typ,alpha=0.3))+geom_line(size=1)+theme_bw()+ggtitle("Wrażliwość ceny opcji na liczbę kroków")+theme(plot.title=element_text(hjust=0.5))+xlab("liczba kroków")
+ggplot(pricing_time2,aes(x=liczba_kroków,y=cena,color=typ))+geom_line(size=1)+theme_bw()+ggtitle("Wrażliwość ceny opcji na liczbę kroków")+theme(plot.title=element_text(hjust=0.5))+xlab("liczba kroków")
 # analiza wrazliwosci 
 ggplot(strike,aes(x = strike))+
   geom_line(aes(y= V2,colour = 'european call'))+
